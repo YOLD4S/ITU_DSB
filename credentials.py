79c61034f960cd549aadb36a34d2340f755c2636
Arif Eren Yoldaş <yoldas21@itu.edu.tr>
2025-06-24 15:42:13 +0300

    Improve credential safety and optimize request timing with ping-based scheduling
    •Introduced credentials.py to isolate sensitive data and prevent accidental modifications in the main program.
    •Added error checks for more robust credential validation before attempting to post.
    •Implemented a pre-check mechanism that validates credentials ahead of time.
    •Optimized request timing by calculating network latency (ping) in advance, allowing the request to be sent precisely when the target system becomes available.

---
+++ credentials.py
@@ -0,0 +1,22 @@
+# This endpoint can be changed every term but not recommended to make change on if you are not sure what you are doing
+URL_POST = 'https://obs.itu.edu.tr/api/ders-kayit/v21'
+
+# Your login credentials to kepler.itu
+USERNAME = ''
+PASSWORD = ''
+
+# Example: ['12345','34563','90075','123132'] (Maximum 10 crn)
+CRNS     = []
+DROPS    = []
+
+# Example format: "15/03/2035 15.00.00" which means 15th March 2035, at 3 pm.
+# The program will most likely raise an error if the input is entered in an incorrect format
+DATETIME = ""
+
+# If you have a stable internet connection, it's recommended to change SEND_EARLY to True and
+# enter a URL which has low latency to ITU servers.
+# This way the automation will decide how earlier to send the request
+# Otherwise the duration that the request is sent earlier will be EARLY(in milliseconds)
+EARLY = 0
+PING_URL = ""
+SEND_EARLY = False
---
+++ project.py
@@ -0,0 +1,119 @@
+from requests import get, post, Session
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from ping3 import ping
+from credentials import *
+import re, sys
+
+
+def main():
+    check_credentials()
+
+    # Wait until 45 seconds left for the system to open
+    start = time_resolver(DATETIME)
+    wait_until(start - timedelta(seconds=45))
+    # Prepare variables to avoid unnecessary waiting when the system is open
+    jwt = get_jwt(USERNAME, PASSWORD)
+    delay = calc_delay()
+
+    wait_until(start - delay)
+    response = post_kepler(jwt, CRNS, DROPS)
+    print(response.text)
+
+
+# Adding the required auth token, posts the request to take or drop lectures
+def post_kepler(jwt, crns, drops):
+    response = post(URL_POST,
+                    headers={'Authorization': 'Bearer {}'.format(jwt)},
+                    json={"ECRN": crns, "SCRN": drops})
+    return response
+
+# Logs in with username and password, returns jwt token
+def get_jwt(username, password):
+    session = Session()
+
+    # Finds the required url after some redirects
+    response = session.get('https://obs.itu.edu.tr') # For hidden inputs
+    login_url = response.url
+
+    # Finds hidden inputs and adds them into the payload for login
+    soup_login = BeautifulSoup(response.text, 'html.parser')
+    login_tokens = soup_login.find_all('input')
+
+    payload = {}
+    # Fills the payload
+    for token in login_tokens:
+        match_username = re.search(r".*username.*", token['id'], re.IGNORECASE)
+        match_password = re.search(r".*password.*", token['id'], re.IGNORECASE)
+        if match_username:
+            payload[token['name']] = username
+        elif match_password:
+            payload[token['name']] = password
+        elif token.has_attr('value'):
+            payload[token['name']] = token['value']
+    # Logs in
+    res_log = session.post(login_url, data=payload)
+    # Checks if the user is redirected meaning that the credentials are valid
+    if res_log.history[0].status_code != 302:
+        sys.exit('Please check your login credentials')
+    # Gets the required cookie to get jwt
+    cookie_jwt = res_log.history[-1].headers.get('Set-Cookie')
+    headers_jwt = {
+        'Cookie': cookie_jwt
+    }
+    # Gets jwt
+    jwt = session.get('https://obs.itu.edu.tr/ogrenci/auth/jwt', headers=headers_jwt).text
+    return jwt
+
+
+def time_resolver(date_time):
+    matches = re.search(r"(\d{2})/(\d{2})/(\d{4}) *(\d{2})\.(\d{2})\.(\d{2})", date_time)
+    if not matches:
+        sys.exit('Please check your date and time format')
+
+    day = int(matches.group(1))
+    month = int(matches.group(2))
+    year = int(matches.group(3))
+    hour = int(matches.group(4))
+    minute = int(matches.group(5))
+    second = int(matches.group(6))
+
+    if 0 <= day <= 30\
+        or 0 <= month <= 12\
+        or 0 <= hour <= 24\
+        or 0 <= minute <= 59\
+        or 0 <= second <= 59:
+        pass
+    else:
+        sys.exit('Please check your date and time')
+    return datetime(year, month, day, hour, minute, second)
+
+
+def wait_until(target_time):
+    while True:
+        now = datetime.now()
+        if now >= target_time:
+            break
+
+
+def calc_delay():
+    if not SEND_EARLY:
+        return timedelta(milliseconds=EARLY)
+
+    if ping(PING_URL):
+        latency = ping(PING_URL)
+    else:
+        sys.exit("Please check PING_URL or if you don't have a valid URL, change SEND_EARLY to False")
+    for i in range(5):
+        temp = ping(PING_URL)
+        latency = max(latency, temp)
+    return latency*1000 + 10
+
+
+def check_credentials():
+    calc_delay()  # To precheck if the url is valid(pingable)
+    get_jwt(USERNAME, PASSWORD)
+
+
+if __name__ == "__main__":
+    main()
